3713162588239f2dd0b7f934dcba13da76f8cd61
Zhen Yi Pan <zharnite@gmail.com>
2019-05-19 15:30:57 -0400

    decided to manually change navbar for every page

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,15 @@
+var path = window.location.pathname;
+var page = path.split("/").pop();
+
+
 $(function(){
 	$("#nav-placeholder").load("nav.html");
 });
+
+var newWin = window.open("nav.html");
+newWin.onload = function(){
+	var nav = newWin.document.getElementById('navbar-id');
+	var htmlPageName = document.getElementById(page);
+	$(nav).find(".active").removeClass("active");
+	$(htmlPageName).parent().addClass("active");
+};
